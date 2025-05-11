195d3d19c0cacc63c657b72c4e062b51f2a97b9a
Zebyan <magdausebi@gmail.com>
2025-05-11 18:11:02 +0300

    Deployed App

--- alembic/env.py
+++ alembic/env.py
@@ -1,5 +1,5 @@
 from logging.config import fileConfig
-
+import os 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
@@ -9,8 +9,9 @@ from app.config import settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
+db_url = os.getenv("DATABASE_URL")
 config = context.config
-config.set_main_option("sqlalchemy.url", f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}')
+config.set_main_option("sqlalchemy.url", db_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
--- app/database.py
+++ app/database.py
@@ -4,7 +4,10 @@ from sqlalchemy.orm import sessionmaker
 from . config import settings
 import os
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+#SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+#engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 MySession = sessionmaker(autocommit=False, autoflush=False, bind=engine)
