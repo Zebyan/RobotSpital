32e02a07ca4dbe129417210638468075dda9b062
Zebyan <magdausebi@gmail.com>
2025-06-02 13:37:39 +0300

    Edit/Delete prescriptii

--- app/routers/prescriptii.py
+++ app/routers/prescriptii.py
@@ -49,3 +49,40 @@ def adaugare_prescriptie(prescriptii: schemas.Creare_Prescriptii, db: Session = 
     db.refresh(db_prescriptii)
     return {"message":"Prescriptia a fost creata!"}
 
+@router.put("/{id_prescriptie}",response_model=schemas.Creare_Prescriptii, status_code=status.HTTP_202_ACCEPTED)
+def editare_pacient(id_prescriptie: int, prescriptie: schemas.Prescriptii, db: Session = Depends(get_db),
+                    access: schemas.Token_Data = Depends(oath2.get_current_angajat)):
+    if not utils.verify_medic_rol(access.rol):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail = "Nu aveti access!"
+        )
+    
+    db_prescriptii = db.query(models.Prescriptii).filter(models.Prescriptii.id_prescriptie == id_prescriptie).first()
+    if db_prescriptii is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Prescriptia nu exista!"
+        )
+    for field, value in prescriptie.dict(exclude_unset=True).items():
+        setattr(db_prescriptii, field, value)
+    db.commit()
+    db.refresh(db_prescriptii)
+    return db_prescriptii
+
+@router.delete("/{id_prescriptie}", status_code=status.HTTP_204_NO_CONTENT)
+def sterge_prescriptie(id_prescriptie: int, db: Session = Depends(get_db),
+                    access: schemas.Token_Data = Depends(oath2.get_current_angajat)):
+    if not utils.verify_medic_rol(access.rol):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail = "Nu aveti access!"
+        )
+    db_prescriptii = db.query(models.Prescriptii).filter(models.Prescriptii.id_prescriptie == id_prescriptie).first()
+    if not db_prescriptii:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Pacientul nu exista!!")
+    
+    db.delete(id_prescriptie)
+    db.commit()
+    return{"mesaj": "Prescriptia a fost stearsa!"}
+
